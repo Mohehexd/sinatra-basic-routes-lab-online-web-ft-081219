8cfce73f2e6245564e7a282a2e8aaddb323b94c0
Guillermo Martinez <dumblemoe@gmail.com>
2019-09-30 21:30:37 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Base
   get '/hometown' do
     "My hometown is Wonderland"
   end
-  get '/favorite song' do
+  get '/favorite-song' do
     "My favorite song is Drift Away"
   end
 end
